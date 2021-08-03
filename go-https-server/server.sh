26a193f2c2b2695402e37b502173d4d659a3fdf7
Ryan Min <ryanmin42@gmail.com>
2021-08-03 11:38:12 -0400

    Use SANs for openssl create command

--- go-https-server/server.sh
+++ go-https-server/server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -f key.pem cert.pem
-yes | openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes -subj '/CN=localhost'
+yes | openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes -subj '/CN=localhost' -extensions SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+            <(printf "[SAN]\nsubjectAltName='DNS:localhost'"))
 
 if [[ $# -ge 1 ]]; then
     go run server.go $1
